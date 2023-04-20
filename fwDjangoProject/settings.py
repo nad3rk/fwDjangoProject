ee8d3b0a99eea6cdc2675b06534298c5ad1e2da6
nad3rk <nader.kaake@gmail.com>
2023-04-20 18:37:18 +0200

    Dockerized project

--- fwDjangoProject/settings.py
+++ fwDjangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o5hz&y2e6w(329s-rfec7j22vn)+gm&&zu#6k#xwypt00z(r^9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
